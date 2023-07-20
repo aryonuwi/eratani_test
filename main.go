b47f78d08f3444fd29e97b5b7cae0a0dc41be757
aryonuwi <aryo1291@gmail.com>
2023-07-21 06:44:47 +0700

    adjust command

--- main.go
+++ main.go
@@ -8,19 +8,22 @@ import (
 )
 
 func main() {
-	// soal no 1
+	// Soal no 1
 	intData := [11]int{10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1000}
 	for _, val := range intData {
 		result := soalsatu.Polycarp(val)
 		fmt.Println(result)
 	}
 	fmt.Println("+============================================+")
-	// soal no 2
+	// Soal no 2
 	testDataPalindrome := [3]interface{}{"kodokkodok", "jinggaaku", 123321}
 	for _, val := range testDataPalindrome {
 		palindrome := soaldua.Plindrome(val)
 		fmt.Println(palindrome)
 	}
 	fmt.Println("+============================================+")
+	// Soal no 4
 
+	fmt.Println("+============================================+")
+	// Soal no 3
 }
--- soalsatu/polycrap.go
+++ soalsatu/polycrap.go
@@ -1,22 +1,15 @@
 package soalsatu
 
 func Polycarp(k int) int {
-	// Initialize variables
 	currentNumber := 1
 	count := 0
-
 	for {
-		// Check if the current number meets the conditions
 		if currentNumber%3 != 0 && currentNumber%10 != 3 {
 			count++
 		}
-
-		// If we reach the k-th element, return it
 		if count == k {
 			return currentNumber
 		}
-
-		// Move to the next number
 		currentNumber++
 	}
 }
